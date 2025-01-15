2d1a32935275bbe8d94838b2a9a55eb95f1b2cf6
Dervey Dylan <114241438+derveydylan@users.noreply.github.com>
2025-01-15 15:29:52 +0100

    feat: SchoolReportDetails page unit tests
    Updates the component code.
    Add data to an apprentice in getApprenticeData().

--- frontend/src/pages/SchoolReportDetails.js
+++ frontend/src/pages/SchoolReportDetails.js
@@ -24,36 +24,26 @@ const SchoolReportDetails = () =>
     const [apprenticeSchoolReport, setApprenticeSchoolReport] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
+    /**
+     * Fetches school report details data.
+     *
+     * @returns {void}
+     *
+     */
+    const fetchSchoolReportData = async () =>
+    {
+        /* TODO : Add user course ID as param */
+        const data = await getApprenticeSchoolReport();
+
+        setApprenticeSchoolReport(data);
+
+        setIsLoading(false);
+    }
+
     useEffect(() =>
     {
-        /**
-         * Fetches school report details data.
-         *
-         * @returns {void}
-         *
-         */
-        const fetchData = async () =>
-        {
-            try
-            {
-                /* TODO : Add user course ID as param */
-                const data = await getApprenticeSchoolReport();
-                setApprenticeSchoolReport(data);
-            }
-
-            catch(error)
-            {
-                console.error("Erreur lors de l'affichage des données.", error)
-            }
-
-            finally
-            {
-                setIsLoading(false);
-            }
-        }
-
-        fetchData();
-    }, [])
+        fetchSchoolReportData();
+    }, []);
 
     return (
         <>
@@ -61,25 +51,38 @@ const SchoolReportDetails = () =>
                 <Loading />
             :
                 <>
-                    <h1>
+                    <h1 data-testid="school-report-details-title">
                         {t("school_report_details", { ns: "titles" })}<br/>
                         {apprenticeSchoolReport?.username}
                     </h1>
 
-                    {/* TODO : Let only trainers and admins see this button */}
-                    <div className="w-max h-max mx-auto my-6">
-                        <Link to={"/list"} className="btn-primary">
+                    <div
+                        className="w-max h-max mx-auto my-6"
+                        data-testid="back-to-list-button-container"
+                    >
+                        <Link
+                            to={"/list"}
+                            className="btn-primary"
+                            data-testid="back-to-list-button"
+                        >
                             {t("back_to_list", { ns: "buttons" })}
                         </Link>
                     </div>
 
-                    {apprenticeSchoolReport && apprenticeSchoolReport.user_course ?
+                    {apprenticeSchoolReport
+                        && apprenticeSchoolReport.user_course
+                        && apprenticeSchoolReport.user_course.teaching_domains
+                    ?
                         <>
                             <Apprentice apprentice={apprenticeSchoolReport} />
 
-                            {apprenticeSchoolReport.user_course.teaching_domains.map(
-                                teachingDomain => <TeachingDomain key={teachingDomain.id} teachingDomain={teachingDomain} />
-                            )}
+                            {apprenticeSchoolReport.user_course.teaching_domains.map(teachingDomain =>
+                            (
+                                <TeachingDomain
+                                    key={teachingDomain.id}
+                                    teachingDomain={teachingDomain}
+                                />
+                            ))}
 
                             <AnnualAverage userCourse={apprenticeSchoolReport.user_course} />
                         </>
--- frontend/src/tests/utils/getApprenticeData.js
+++ frontend/src/tests/utils/getApprenticeData.js
@@ -10,14 +10,34 @@ export const getApprenticeData = () =>
         "user_id": 1,
         "username": "Testing Apprentice",
         "fk_trainer": 1,
-        "user_courses":
-        [
-            {
-                "id": 1,
-                "course_plan_id": 1,
-                "official_name": "The official name for a test course plan.",
-                "global_average": 1.0
-            }
-        ]
+        "user_course":
+        {
+            "id": 1,
+            "course_plan_id": 1,
+            "official_name": "The official name for a test course plan.",
+            "global_average": 1.0,
+            "teaching_domains":
+            [
+                {
+                    "id": 1,
+                    "title": "Testing Teaching Domain",
+                    "weight": 1,
+                    "average": 1.0,
+                    "is_eliminatory": false,
+                    "subjects":
+                    [
+                        {
+                            "id": 1,
+                            "name": "Testing Teaching Subject",
+                            "average": 1,
+                            "grades":
+                            [
+                                { "id": 1, "grade": 1 }
+                            ]
+                        }
+                    ]
+                },
+            ]
+        }
     }
 }
